95ff01b46c6756ca8c15de9702825193b7d9f30a
sda134 <saji.sus4@gmail.com>
2024-12-07 11:29:28 +0900

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',    // すべてのIPv4アドレスでリッスン
+    port: 4321
+  },
+
+  integrations: [tailwind()]
+});
---
+++ src/content/events/star_gaze.md
@@ -0,0 +1,10 @@
+---
+title: "星を見る会"
+date: "2024-12-11"
+time: "14:00-16:00"
+location: "伊佐治家"
+description: "星を感じましょう"
+image: "/images/events/2024-new-year-party.jpg"
+---
+
+# 星を見る会(md)
