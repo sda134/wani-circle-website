516e1a1a559c4cecd1aaed78fa6943862ff1a601
sda134 <saji.sus4@gmail.com>
2024-12-22 12:37:13 +0900

    modified:applied whole color(test1)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+
+
+			colors: {
+				// やさしい緑系のカラーパレット
+				primary: {
+				  light: '#e0f2e9',
+				  DEFAULT: '#98d4b9',
+				  dark: '#5c9f8f',
+				},
+				secondary: {
+				  light: '#f0f7f4',
+				  DEFAULT: '#b7d9cb',
+				  dark: '#7ab5a0',
+				},
+			}
+			
+
+
+
+		},
 	},
 	plugins: [],
 }
